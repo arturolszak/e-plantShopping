c7f35241680b121bf46a8561aa58f6be7bee9c58
Artur Olszak <aolszak@gmail.com>
2025-03-31 17:41:46 -0400

    fixed styling issues

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -300,14 +300,14 @@ function ProductList({ onHomeClick }) {
                 <div className="product-grid">
                     {plantsArray.map((category, index) => (
                     <div key={index}>
-                        <h1><div>{category.category}</div></h1>
+                        <h1 class="plantname_heading"><div class="plant_heading">{category.category}</div></h1>
                         <div className="product-list">
                             {category.plants.map((plant, plantIndex) => (
                             <div className="product-card" key={plantIndex}>
                                 <img className="product-image" src={plant.image} alt={plant.name} />
                                 <div className="product-title">{plant.name}</div>
-                                <div>{plant.description}</div>
                                 <div className="product-price">{plant.cost}</div>
+                                <div>{plant.description}</div>
                                 <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                             </div>
                             ))}
